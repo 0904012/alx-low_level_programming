c0f2b2cfb562c984eb6c9168e7767bb188c6d226
0904012 <dawitgetu0@gmail.com>
2022-03-13 00:41:57 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,11 @@
 
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	
+/*
+ * print exactly the written program
+ */
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
