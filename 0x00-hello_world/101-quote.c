f7c4b36d67c2b397d9bf63a50006539ce10163b9
0904012 <dawitgetu0@gmail.com>
2022-05-05 03:36:00 -0700

    0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,9 @@
  * Return:1
  */
 int main(void)
+
 {
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, ch, sizeof(ch));
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
 
